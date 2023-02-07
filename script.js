246e8e654736682c6e133ccc6d26ed0e16bcca60
Luis Lima <37217235+llima91@users.noreply.github.com>
2023-02-06 21:43:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+//variável é um espaço do computador onde alocamos algo (número, letra, texto, etc.)
+//função é um trecho de código que só é executado quando é chamado.
+
+let chave = "0bba744b5c955b208973278cc0e3345f"
+
+function colocarNaTela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade: " + Math.floor(dados.main.humidity) + "%"
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+}
+
+async function buscarCidade(cidade) {
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
+        cidade +
+        "&appid=" +
+        chave +
+        "&lang=pt_br" +
+        "&units=metric"
+    )
+        .then(resposta => resposta.json())
+
+    colocarNaTela(dados)
+}
+
+function cliqueiNoBotao() {
+    let cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
